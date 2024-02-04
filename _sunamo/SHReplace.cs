cc30a4cfef8cbfe27bc39c4c1ea36d369ca37292
sunamo.cz_asus <radek.jancik@sunamo.cz>
2024-02-04 12:09:17 +0100

    .

--- _sunamo/SHReplace.cs
+++ _sunamo/SHReplace.cs
@@ -5,53 +5,52 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-namespace SunamoXml._sunamo
+namespace SunamoXml._sunamo;
 
+
+internal class SHReplace
 {
-    internal class SHReplace
+    internal static string ReplaceAllDoubleSpaceToSingle(string text, bool alsoHtml = false)
     {
-        internal static string ReplaceAllDoubleSpaceToSingle(string text, bool alsoHtml = false)
+        //text = SHSH.FromSpace160To32(text);
+
+        if (alsoHtml)
         {
-            //text = SHSH.FromSpace160To32(text);
-
-            if (alsoHtml)
-            {
-                text = text.Replace(" &nbsp;", " ");
-                text = text.Replace("&nbsp; ", " ");
-                text = text.Replace("&nbsp;", " ");
-            }
-
-            while (text.Contains(AllStrings.doubleSpace))
-            {
-                text = text.Replace(AllStrings.doubleSpace, AllStrings.space);// ReplaceAll2(text, AllStrings.space, );
-            }
-
-            // Here it was cycling, dont know why, therefore without while
-            //while (text.Contains(AllStrings.doubleSpace16032))
-            //{
-            //text = ReplaceAll2(text, AllStrings.space, AllStrings.doubleSpace16032);
-            //}
-
-            //while (text.Contains(AllStrings.doubleSpace32160))
-            //{
-            //text = ReplaceAll2(text, AllStrings.space, AllStrings.doubleSpace32160);
-            //}
-
-            return text;
+            text = text.Replace(" &nbsp;", " ");
+            text = text.Replace("&nbsp; ", " ");
+            text = text.Replace("&nbsp;", " ");
         }
 
-
-        internal static string ReplaceAllWhitecharsForSpace(string c)
+        while (text.Contains(AllStrings.doubleSpace))
         {
-            foreach (var item in AllChars.whiteSpacesChars)
-            {
-                if (item != AllChars.space)
-                {
-                    c = c.Replace(item, AllChars.space);
-                }
-            }
-
-            return c;
+            text = text.Replace(AllStrings.doubleSpace, AllStrings.space);// ReplaceAll2(text, AllStrings.space, );
         }
+
+        // Here it was cycling, dont know why, therefore without while
+        //while (text.Contains(AllStrings.doubleSpace16032))
+        //{
+        //text = ReplaceAll2(text, AllStrings.space, AllStrings.doubleSpace16032);
+        //}
+
+        //while (text.Contains(AllStrings.doubleSpace32160))
+        //{
+        //text = ReplaceAll2(text, AllStrings.space, AllStrings.doubleSpace32160);
+        //}
+
+        return text;
+    }
+
+
+    internal static string ReplaceAllWhitecharsForSpace(string c)
+    {
+        foreach (var item in AllChars.whiteSpacesChars)
+        {
+            if (item != AllChars.space)
+            {
+                c = c.Replace(item, AllChars.space);
+            }
+        }
+
+        return c;
     }
 }
