0e75268e2100df5d99cd30f1affb29bf4ae81b41
sunamo.cz <radek.jancik@sunamo.cz>
2024-10-16 12:28:35 +0200

    Removed unused file

--- _sunamo/WhitespaceCharService.cs
+++ _sunamo/WhitespaceCharService.cs
@@ -1,9 +1,6 @@
 namespace SunamoXml._sunamo;
 using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 internal class WhitespaceCharService
 {
