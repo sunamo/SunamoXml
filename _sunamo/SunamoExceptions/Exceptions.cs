db0238815bed09019ad10aa86d89cd9bd5025bcb
sunamo.cz <radek.jancik@sunamo.cz>
2025-01-29 22:40:12 +0100

    Remove remain unused method

--- _sunamo/SunamoExceptions/Exceptions.cs
+++ _sunamo/SunamoExceptions/Exceptions.cs
@@ -75,40 +75,8 @@ bool fillAlsoFirstTwo = true)
     #endregion
 
     #region IsNullOrWhitespace
-    internal static string? IsNullOrWhitespace(string before, string argName, string argValue, bool notAllowOnlyWhitespace)
-    {
-        string addParams;
-        if (argValue == null)
-        {
-            addParams = AddParams();
-            return CheckBefore(before) + argName + " is null" + addParams;
-        }
-        if (argValue == string.Empty)
-        {
-            addParams = AddParams();
-            return CheckBefore(before) + argName + " is empty (without trim)" + addParams;
-        }
-        if (notAllowOnlyWhitespace && argValue.Trim() == string.Empty)
-        {
-            addParams = AddParams();
-            return CheckBefore(before) + argName + " is empty (with trim)" + addParams;
-        }
-        return null;
-    }
     readonly static StringBuilder sbAdditionalInfoInner = new();
     readonly static StringBuilder sbAdditionalInfo = new();
-    internal static string AddParams()
-    {
-        sbAdditionalInfo.Insert(0, Environment.NewLine);
-        sbAdditionalInfo.Insert(0, "Outer:");
-        sbAdditionalInfo.Insert(0, Environment.NewLine);
-        sbAdditionalInfoInner.Insert(0, Environment.NewLine);
-        sbAdditionalInfoInner.Insert(0, "Inner:");
-        sbAdditionalInfoInner.Insert(0, Environment.NewLine);
-        var addParams = sbAdditionalInfo.ToString();
-        var addParamsInner = sbAdditionalInfoInner.ToString();
-        return addParams + addParamsInner;
-    }
     #endregion
 
     #region OnlyReturnString 
